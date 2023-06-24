07b27a031c818a190476d97518be5377a8ab505c
biplabroy1 <biplabroy7318@gmail.com>
2023-06-24 10:43:28 +0530

    Work properlly all the err heldelling

--- signUp/server/controller/signUpController.js
+++ signUp/server/controller/signUpController.js
@@ -2,57 +2,74 @@
 const User = require('../model/signUpModel')
 const chalk = require('chalk')
 
-function isValidString(string) {
-    if (string == undefined || string.length === 0) {
-        return true;
-    } else {
-        return false;
+const postSignUp = async (req, res, next) => {
+    const { name, email, password } = req.body;
+    if (req.body.length === 0) {
+        console.log("Body must be filled")
+        return res
+            .status(400)
+            .send({ status: false, err: "Body must be filled" })
     }
-}
+    if (!name || name.trim().length == 0) {
+        console.log("name is require");
+        return res
+            .status(400)
+            .send({ status: false, err: "name is require" })
+    }
 
+    if (findName) {
+        console.log("Name already exist");
+        return res
+            .status(400)
+            .send({ status: false, err: "Name already exist" })
+    }
 
-const postSignUp = async(req, res, next)=>{
-    const {name, email, password} = req.body;
-    const data = await User.findOne({
-        where:{
-            email:email
+    if (!email || email.trim().length == 0) {
+        console.log("email is require");
+        return res
+            .status(400)
+            .send({ status: false, err: "email is require" })
+    }
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!validRegex.test(email)) {
+        console.log("Enter valid email");
+        return res
+            .status(400)
+            .send({ status: false, err: "Enter valid email" })
+    }
+    const findEmail = await User.findOne({
+        where: {
+            email: email
         }
     })
-
-    if(data){
-        console.log(chalk.red("Email Id Already exist"));
-        return res.status(400).send({ status: false, msg: "EmailId Is Already Exist In DB" })
-        
+    if (findEmail) {
+        console.log("Email already exist");
+        return res
+            .status(400)
+            .send({ status: false, err: "Email already exist" })
     }
-    else if(isValidString(name) || isValidString(email) || isValidString(password)){
-        res.status(400).json({err: 'Please fill properlly in your form'})
-        console.log(chalk.red.inverse('Please fill properlly in your form'));
+    if (!password || password.trim().length == 0) {
+        console.log("require password");
+        return res
+            .status(400)
+            .send({ status: false, err: "require password" })
     }
-    else{
-        try{
-
-            const postData = await User.create({
-                name,
-                email,
-                password
-            })
-            try{
-                res.status(201).send("Your Profile Successfully Created")
-                console.log(chalk.cyan(`Yours Details  Name: ${name} - Email: ${email} - Password : ${password}`));
-            }
-            catch(err){
-                console.log(chalk.red(err.message));
-            }
-        }
-        catch(err){
-            console.log(chalk.red(err.message));
-        }
+ 
+    const postData = await User.create({
+        name,
+        email,
+        password
+    })
+    try {
+        res.status(201).send("Your Profile Successfully Created")
+        console.log(chalk.cyan(`Yours Details  Name: ${name} - Email: ${email} - Password : ${password}`));
     }
-    
+    catch (err) {
+        console.log(chalk.red(err.message));
+    }
 }
 
-
-
+``
 //PAGE NOT FOUND
 const pageNotFound = (req, res, next) => {
     res.status(404).send('<h1>Page Not Found</h1>')
